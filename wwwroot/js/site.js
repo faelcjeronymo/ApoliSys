781b7d6de46110021be8bd72d3d1f02246363f10
faelcjeronymo <rafaelcjeronymo@outlook.com>
2022-11-09 14:45:40 -0300

    select

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -21,6 +21,13 @@ function avancar_etapa_formulario() {
 
         console.log(form);
 
+        //Validando Selects
+        $(".selectize-control").each(function(index, elem){
+            if ($(elem).find(".item").attr('data-value') == null) {
+                console.log("Teste");
+            }                
+        });
+
         if (!form.checkValidity()) {
             $(form).addClass("was-validated");
             abrir_toast();
@@ -29,10 +36,12 @@ function avancar_etapa_formulario() {
     })
 }
 
-function abrir_toast() {
+function abrir_toast(titulo = "Titulo", icone, conteudo, posicao) {
+
     let toast = `
     <div class="toast position-absolute top-0 start-50 translate-middle-x" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-header">
+            <i class="fa fa-exclamation-circle"></i>
             <strong class="me-auto">Bootstrap</strong>
             <small class="text-muted">11 mins ago</small>
             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
@@ -55,7 +64,12 @@ function abrir_toast() {
 }
 
 function iniciar_selectize() {
-    $("select").selectize({
+    let selectize = $("select").selectize({
         placeholder: "Selecione",
+        allowEmptyOption: true,
     });
+
+    selectize.on("open", () => {
+        $(this).clear();
+    })
 }
