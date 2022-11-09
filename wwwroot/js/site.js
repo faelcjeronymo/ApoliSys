34e9e84cd09126b4e74e7c72b1ecdff11108c08e
faelcjeronymo <rafaelcjeronymo@outlook.com>
2022-11-09 02:16:23 -0300

    Cadastro de segurados

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,13 +5,57 @@
 $(window).on("load", function () {
     aplicar_mascaras_formatacao();
     iniciar_selectize();
+    avancar_etapa_formulario();
 });
 
 function aplicar_mascaras_formatacao() {
     $("input[name='CpfCnpj']").mask("000.000.000-00");
+    $("input[name='Cnh']").mask("00000000000");
+    $("input[name='Celular']").mask("(00) 00000-0000");
     $("input[name='Cep']").mask("00000-000");
 }
 
+function avancar_etapa_formulario() {
+    $("form.step-form button").on("click", function () {
+        let form = $("form.step-form")[0];
+
+        console.log(form);
+
+        if (!form.checkValidity()) {
+            $(form).addClass("was-validated");
+            abrir_toast();
+        }
+
+    })
+}
+
+function abrir_toast() {
+    let toast = `
+    <div class="toast position-absolute top-0 start-50 translate-middle-x" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header">
+            <strong class="me-auto">Bootstrap</strong>
+            <small class="text-muted">11 mins ago</small>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body">
+            Hello, world! This is a toast message.
+        </div>
+    </div>
+    `;
+
+    $(".card-body .container-fluid").append(toast);
+
+    $(".toast").each((index, elem) => {
+        $(elem).on("hidden.bs.toast", () => {
+            $(".toast").remove();
+        })
+        elem = new bootstrap.Toast(elem);
+        elem.show();
+    })
+}
+
 function iniciar_selectize() {
-    $("select").selectize();
+    $("select").selectize({
+        placeholder: "Selecione",
+    });
 }
